67687b9fc3cc0b614bd41ab82ab91945ff33071a
Joel Spencer <matingara@gmail.com>
2021-08-11 11:56:58 +1000

    Initial commit

---
+++ Initial/ALrequestTemplates.py
@@ -0,0 +1,14 @@
+#! /Library/Frameworks/Python.framework/Versions/3.8/bin/python3
+import requests, json
+
+#r = requests.get('https://192.168.0.237/FireFlow/api/templates/', verify=False)
+r = requests.get('https://10.178.4.51/FireFlow/api/templates/', verify=False)
+
+session = requests.Session()
+
+print('r.text', r.text)
+print('r: ', r)
+print('STATUS CODE: ', r.status_code)
+print('HEADERS: ', r.headers)
+print('CONTENT: ', r.content)
+print('C1: ', session.cookies, 'C2: ', r.cookies)
